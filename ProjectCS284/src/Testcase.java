bf1ce6c713a7eef01ebbf63b201a1d42fa375b6d
User <User@DESKTOP-33CURNR>
2017-11-10 19:51:29 +0700

    Testcase

---
+++ ProjectCS284/src/Testcase.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Testcase {
+
+	LogInFrame lf;
+	@Before
+    public void setUp(){
+    	lf=new LogInFrame();
+    }
+	@After
+	public void tearDown() {
+		lf=null;
+	
+	}
+	@Test
+	public void testString(){
+		assertNull( "Password or username wrong",lf.data);
+		
+	
+		
+	}
+}
