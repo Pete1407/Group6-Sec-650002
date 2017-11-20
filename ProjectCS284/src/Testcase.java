814049756ea334a9c15de7b4f93ead7477384738
windows <windows@DESKTOP-IU8KH2T>
2017-11-20 18:00:04 +0700

    เพิ่มเติมไฟล์txt ชื่อนามสกุล email เพิ่มtest

--- ProjectCS284/src/Testcase.java
+++ ProjectCS284/src/Testcase.java
@@ -2,26 +2,45 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.Test; 
 
 public class Testcase {
 
-	LogInFrame lf;
+	LogInFrame login;
+	Admin admin;
+	AllSubject all;
 
 	@Before
 	public void setUp() {
-		lf = new LogInFrame();
+		login = new LogInFrame();
+		admin = new Admin();
+		all = new AllSubject();
 	}
 
 	@After
 	public void tearDown() {
-		lf = null;
+		login = null;
 
 	}
 
 	@Test
-	public void testString() {
-		assertNull("Password or username wrong", lf.data);
+	public void notNullLogin() {
+		//§≈“ ‰¡Ë‡ªÁπNull
+		assertNotNull("Password or username wrong", login);
 
 	}
+	
+	@Test
+	public void admintest(){
+		assertNotNull(all.getIndex("CS211"));
+
+	}
+	
+	@Test
+	public void Indextest(){
+		assertTrue(all.getIndex("CS211") ==0);
+		assertTrue(all.getIndex("CS101") ==1);
+
+	}
+	
 }
